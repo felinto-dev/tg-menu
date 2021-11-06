1b1e974ec4db62d1ace8d3cdba9828bb0acbfe1b
Felinto <emersong20.email@gmail.com>
2021-11-06 15:38:34 -0300

    ✨ register cache module

--- lib/tg-menu.module.ts
+++ lib/tg-menu.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 
 import { SetupMenuPathInterceptor } from './interceptors/setup-menu-path.interceptor';
 import { PaginationTelegramService } from './services/pagination.service';
 
 @Module({
+  imports: [CacheModule.register()],
   providers: [PaginationTelegramService, SetupMenuPathInterceptor],
   exports: [PaginationTelegramService, SetupMenuPathInterceptor],
 })

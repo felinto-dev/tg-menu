1a7442fa75540587ce069e11b4940147d4c649a5
Felinto <emersong20.email@gmail.com>
2021-11-07 00:38:00 -0300

    ⚡️ callback query should expire in 2 weeks

--- lib/services/pagination.service.ts
+++ lib/services/pagination.service.ts
@@ -20,7 +20,7 @@ export class PaginationTelegramService {
     const paginationMenu: InlineKeyboardButton[] = [];
 
     let pageSize: number;
-    if (totalAvailableItems > 30) {
+    if (totalAvailableItems / itemsByPage > 3) {
       pageSize = 5;
     } else {
       pageSize = 3;
@@ -31,7 +31,7 @@ export class PaginationTelegramService {
       single: itemsByPage,
       pageSize,
       currentPage,
-      pageLinkRule: (pageNumber: number) => {
+      pageLinkRule: async (pageNumber: number) => {
         const callbackData = `${endpoint}?page=${pageNumber}`;
 
         if (callbackData.length > MAX_ALLOWED_CALLBACK_DATA) {
@@ -49,25 +49,27 @@ export class PaginationTelegramService {
     if (currentPage !== 1) {
       paginationMenu.push({
         text: '« «',
-        callback_data: pagination.firstPage.link,
+        callback_data: await pagination.firstPage.link,
       });
     }
 
-    const paginationKeyboard = pagination.pages.map(
-      (pageItem: { number: number; link: string }) => ({
-        text:
-          pageItem.number === pagination.currentPage
-            ? `- ${pageItem.number} -`
-            : pageItem.number,
-        callback_data: pageItem.link,
-      }),
+    const paginationKeyboard: InlineKeyboardButton[] = await Promise.all(
+      pagination.pages.map(
+        async (pageItem: { number: number; link: Promise<string> }) => ({
+          text:
+            pageItem.number === pagination.currentPage
+              ? `- ${pageItem.number} -`
+              : pageItem.number,
+          callback_data: await pageItem.link,
+        }),
+      ),
     );
     paginationMenu.push(...paginationKeyboard);
 
     if (pagination.lastPage.number !== currentPage) {
       paginationMenu.push({
         text: '» »',
-        callback_data: pagination.lastPage.link,
+        callback_data: await pagination.lastPage.link,
       });
     }
 
--- lib/services/temporary-callback.service.ts
+++ lib/services/temporary-callback.service.ts
@@ -6,13 +6,17 @@ import { Injectable, CACHE_MANAGER, Inject } from '@nestjs/common';
 export class TemporaryCallbackService {
   constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
+  private readonly EXPIRES_IN = 60 * 60 * 24 * 14; // 14 days
+
   async setCallback(callbackData: string) {
     const key = crypto.createHash('sha256').update(callbackData).digest('hex');
-    await this.cacheManager.set(key, callbackData);
+    await this.cacheManager.set(key, callbackData, { ttl: this.EXPIRES_IN });
     return key;
   }
 
   async getCallback(key: string) {
-    return this.cacheManager.get(key);
+    const callbackData = await this.cacheManager.get(key);
+    await this.cacheManager.set(key, callbackData, { ttl: this.EXPIRES_IN });
+    return callbackData;
   }
 }

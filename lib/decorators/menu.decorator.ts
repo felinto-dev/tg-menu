03253f9ef7ef0a7740ea656c71238c8b18051e01
Felinto <emersong20.email@gmail.com>
2021-11-10 18:42:06 -0300

    ♻️ change TGMenuDecorator to only register path

--- lib/decorators/menu.decorator.ts
+++ lib/decorators/menu.decorator.ts
@@ -1,21 +1,6 @@
-import { Action } from 'nestjs-telegraf';
-import {
-  applyDecorators,
-  RequestMethod,
-  SetMetadata,
-  UseInterceptors,
-} from '@nestjs/common';
+import { applyDecorators, SetMetadata } from '@nestjs/common';
 
-import { SetupMenuPathInterceptor } from '../interceptors/setup-menu-path.interceptor';
-import { MenuPathParser } from '../helpers/menu-path-parser.helper';
+import { sanitizeMenuPath } from '../utils/sanitize-menu-path.utils';
 
-export const TGMenu = (requestMethod: RequestMethod, path = '') => {
-  const menuPath = new MenuPathParser(requestMethod, path);
-
-  return applyDecorators(
-    Action(menuPath.templateToRegex()),
-    SetMetadata('menuPath', menuPath),
-    SetMetadata('requestMethod', requestMethod),
-    UseInterceptors(SetupMenuPathInterceptor),
-  );
-};
+export const TGMenu = (path = '/') =>
+  applyDecorators(SetMetadata('menuPath', sanitizeMenuPath(path)));

262924feb100a70f5e440d23ee045f0f5a75c95e
Felinto <emersong20.email@gmail.com>
2021-11-10 23:47:23 -0300

    🚚 rename consts metadata

--- lib/decorators/menu.decorator.ts
+++ lib/decorators/menu.decorator.ts
@@ -1,7 +1,7 @@
 import { applyDecorators, SetMetadata } from '@nestjs/common';
 
-import { TG_CLASS_PATH_METADATA } from '../consts';
+import { TG_ROUTE_PATH_METADATA } from '../consts';
 import { sanitizeMenuPath } from '../utils/sanitize-menu-path.utils';
 
 export const TGMenu = (path = '/') =>
-  applyDecorators(SetMetadata(TG_CLASS_PATH_METADATA, sanitizeMenuPath(path)));
+  applyDecorators(SetMetadata(TG_ROUTE_PATH_METADATA, sanitizeMenuPath(path)));

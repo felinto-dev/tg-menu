6909a580ee77993e53bc961f171b7eca5ff6ed2a
Felinto <emersong20.email@gmail.com>
2021-11-11 23:15:43 -0300

    ✨ add utils to parse path

--- lib/utils/__tests__/path.utils.spec.ts
+++ lib/utils/__tests__/path.utils.spec.ts
@@ -1,4 +1,5 @@
-import { sanitizeMenuPath } from '../path.utils';
+import { RequestMethod } from '@nestjs/common';
+import { generatePathRegex, parsePath, sanitizeMenuPath } from '../path.utils';
 
 describe(sanitizeMenuPath.name, () => {
   it('should throws an error when have does not have slashes in beginner or end', () => {
@@ -9,5 +10,66 @@ describe(sanitizeMenuPath.name, () => {
 
   it('should throw an error when path is invalid', () => {
     expect(() => sanitizeMenuPath('producer//accounts')).toThrowError();
+  });
+});
+
+describe(generatePathRegex.name, () => {
+  it('should match simple callback URL style', () => {
+    expect('GET /producer/products/').toMatch(
+      generatePathRegex(RequestMethod.GET, '/producer/products/'),
+    );
+    expect('POST /producer/products/').toMatch(
+      generatePathRegex(RequestMethod.POST, '/producer/products/'),
+    );
+    expect('POST /producer/products/').not.toMatch(
+      generatePathRegex(RequestMethod.GET, '/producer/products/'),
+    );
+  });
+
+  it('should match callback with path parameters', () => {
+    expect('GET /producer/products/1/').toMatch(
+      generatePathRegex(RequestMethod.GET, '/producer/products/:productId/'),
+    );
+    expect('POST /producer/products/123/456/').toMatch(
+      generatePathRegex(
+        RequestMethod.POST,
+        '/producer/products/:productId/:groupId/',
+      ),
+    );
+    expect('POST /producer/products/1/').not.toMatch(
+      generatePathRegex(RequestMethod.GET, '/producer/products/:productId/'),
+    );
+  });
+
+  it('should match callback with query parameters', () => {
+    expect('GET /producer/products/?page=1').toMatch(
+      generatePathRegex(RequestMethod.GET, '/producer/products/'),
+    );
+    expect('GET /producer/products/?page=1&sortBy=downloads').toMatch(
+      generatePathRegex(RequestMethod.GET, '/producer/products/'),
+    );
+  });
+});
+
+describe(parsePath.name, () => {
+  it('should parse path parameters', () => {
+    expect(
+      parsePath(
+        'GET /producer/products/1/',
+        'GET /producer/products/:productId/',
+      ).params,
+    ).toStrictEqual({
+      productId: '1',
+    });
+
+    expect(
+      parsePath(
+        'GET /producer/products/123/456/',
+        'GET /producer/products/:productId/:groupId/',
+      ).params,
+    ).toStrictEqual({
+      productId: '123',
+      groupId: '456',
+    });
   });
 });
--- lib/utils/path.utils.ts
+++ lib/utils/path.utils.ts
@@ -1,11 +1,41 @@
+import { RequestMethod } from '@nestjs/common';
+import { pathToRegexp, match } from 'path-to-regexp';
+
+const queryParametersRegex = /(\?|&)([^=]+)=([^&/]+)/;
+
 export const sanitizeMenuPath = (path: string) => {
   if (!path.startsWith('/') || !path.endsWith('/')) {
     throw new Error("Paths should to start and ends with '/' (slash)");
   }
 
   if (path.match(/\/\//)) {
-    throw new Error("Paths can't to have double slash");
+    throw new Error('Paths cannot to have double slash');
   }
 
   return path;
+};
+
+export const generatePathRegex = (
+  requestMethod: RequestMethod,
+  path: string,
+) => {
+  path = sanitizeMenuPath(path);
+  const pathRegex = pathToRegexp(path, [], {
+    start: false,
+    end: false,
+    strict: true,
+  });
+  return new RegExp(
+    `^${RequestMethod[requestMethod]} ${pathRegex.source}(<queryParameters>${queryParametersRegex.source})?`,
+  );
+};
+
+export const parsePath = (rawPath: string, path: string) => {
+  const pathMatch = match(path);
+
+  return {
+    // eslint-disable-next-line dot-notation
+    params: { ...pathMatch(rawPath)['params'] },
+    query: {},
+  };
 };
